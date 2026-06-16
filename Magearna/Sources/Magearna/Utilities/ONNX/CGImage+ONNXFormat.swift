bd711b4d88b8afa2660d3a46b345bb8bacf0129c
lucka-me <i@lucka.moe>
2026-06-16 16:53:07 +0800

    Add CGImage.decodeForONNX(means:scales:) - Support different aeguments for each channel

--- Magearna/Sources/Magearna/Utilities/ONNX/CGImage+ONNXFormat.swift
+++ Magearna/Sources/Magearna/Utilities/ONNX/CGImage+ONNXFormat.swift
@@ -27,6 +27,8 @@ extension CGImage {
         )
         
         // cv2.dnn.blobFromImage(image, scale, size, (mean, mean, mean), swapRB=True)
+        // cv2: ([0, 255] - mean) * scale
+        // vImage: [0, 1] * scale + bias
         imageBuffer.applyGamma(
             linearParameters: (scale: scale * 255, bias: -mean * scale),
             exponentialParameters: (1, 0, 1, 0),
@@ -42,6 +44,44 @@ extension CGImage {
             .planarBuffers()
             .reduce(into: Data()) { partial, buffer in
                 buffer.array.withUnsafeBufferPointer { pointer in
+                    partial.append(.init(buffer: pointer))
+                }
+            }
+    }
+    
+    func decodeForONNX(means: [ 3 of Float], scales: [ 3 of Float]) throws -> Data {
+        var imageFormat = vImage_CGImageFormat(
+            bitsPerComponent: 32,
+            bitsPerPixel: 32 * Self.pixelFormat.channelCount,
+            colorSpace: .init(name: CGColorSpace.sRGB)!,
+            bitmapInfo: .init(
+                alpha: .none,
+                component: .float,
+                byteOrder: .order32Host,    // NOT .orderDefault
+                pixelFormat: .packed
+            )
+        )!
+        let imageBuffer = try vImage.PixelBuffer(
+            cgImage: self,
+            cgImageFormat: &imageFormat,
+            pixelFormat: Self.pixelFormat
+        )
+        
+        return imageBuffer
+            .planarBuffers()
+            .enumerated()
+            .reduce(into: Data()) { partial, buffer in
+                buffer.element.applyGamma(
+                    linearParameters: (
+                        scale: scales[buffer.offset] * 255,
+                        bias: -means[buffer.offset] * scales[buffer.offset]
+                    ),
+                    exponentialParameters: (1, 0, 1, 0),
+                    boundary: 2.0,
+                    destination: buffer.element
+                )
+                
+                buffer.element.array.withUnsafeBufferPointer { pointer in
                     partial.append(.init(buffer: pointer))
                 }
             }
