f537b083abc637f3b585cea23a6ef59c701fe502
lucka-me <i@lucka.moe>
2026-07-13 23:26:48 +0800

    Fix missing sources for Xcode Cloud

--- ci_scripts/ci_post_clone/ci_prepare_onnxruntime.sh
+++ ci_scripts/ci_post_clone/ci_prepare_onnxruntime.sh
@@ -8,8 +8,8 @@ then
     mkdir -p $buildDirectory
 fi
 
+echo 'Fetching XCFramework'
 artifactURL=https://api.github.com/repos/lucka-me/michina-swift/actions/artifacts/8282040286/zip
-
 curl -s -S -L $artifactURL                                              \
     -H "Authorization: Bearer $GITHUB_ACTIONS_ARTIFACTS_DOWNLOAD_TOKEN" \
     | aa extract -d $buildDirectory
--- ci_scripts/ci_post_clone/ci_prepare_tokenizers.sh
+++ ci_scripts/ci_post_clone/ci_prepare_tokenizers.sh
@@ -2,13 +2,22 @@
 
 set -e
 
-targetPath=$CI_PRIMARY_REPOSITORY_PATH/Tokenizers/Rust/target
+packagePath=$CI_PRIMARY_REPOSITORY_PATH/Tokenizers
+
+targetPath=$packagePath/Rust/target
 if [ ! -d $targetPath ]
 then
     mkdir -p $targetPath
 fi
 
+echo 'Fetching XCFramework'
 artifactURL=https://api.github.com/repos/lucka-me/michina-swift/actions/artifacts/8281541814/zip
 curl -s -S -L $artifactURL                                              \
     -H "Authorization: Bearer $GITHUB_ACTIONS_ARTIFACTS_DOWNLOAD_TOKEN" \
     | aa extract -d $targetPath
+
+echo 'Fetching Generated Sources'
+artifactURL=https://api.github.com/repos/lucka-me/michina-swift/actions/artifacts/8283814710/zip
+curl -s -S -L $artifactURL                                              \
+    -H "Authorization: Bearer $GITHUB_ACTIONS_ARTIFACTS_DOWNLOAD_TOKEN" \
+    | aa extract -d $packagePath/Sources/Tokenizers
