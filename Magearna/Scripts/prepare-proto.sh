81f1c94466110ed271f9b7df21d11ee91156d3da
lucka-me <i@lucka.moe>
2026-07-18 23:05:54 +0800

    Fix onnx proto source path

--- Magearna/Scripts/prepare-proto.sh
+++ Magearna/Scripts/prepare-proto.sh
@@ -4,6 +4,6 @@ set -e
 
 repositoryPath=$(realpath $(dirname $0)/../../)
 
-sed 's/^package onnx;$/package onnx;\noption swift_prefix = "ONNX";/'                           \
-    $repositoryPath/ONNXRuntime/onnxruntime/cmake/external/onnx/onnx/onnx.proto3                \
+sed 's/^package onnx;$/package onnx;\noption swift_prefix = "ONNX";/'                               \
+    $repositoryPath/ONNXRuntime/Frameworks/onnxruntime/source/cmake/external/onnx/onnx/onnx.proto3  \
     > $repositoryPath/Magearna/Sources/Magearna/Utilities/ONNX/Protobuf/Definitions/ONNX.proto
